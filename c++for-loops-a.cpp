70f948785fe2e686554c82425f445ce7bdea111c
Gang-Ryung Uh <gangryunguh@gmail.com>
2019-06-03 10:17:38 -0500

    10 loop exercise problems are added

---
+++ c++for-loops-a.cpp
@@ -0,0 +1,104 @@
+//
+// Loop exercises
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    /* prob1:
+       Write a program to find the first 10 natural numbers.
+       Sample output:
+       The natural numbers are:
+       1 2 3 4 5 6 7 8 9 10
+     */
+
+
+    /* prob2:
+       Write a program to find the sum of first 10 natural numbers.
+       The natural numbers are:
+       1 2 3 4 5 6 7 8 9 10
+       The sum of first 10 natural numbers: 55
+     */
+
+
+    /* prob3:
+       Write a program to display n terms of natural number and their sum.
+       Sample Output:
+       Input a number of terms: 7
+       The natural numbers upto 7th terms are:
+       1 2 3 4 5 6 7
+       The sum of the natural numbers is: 28
+     */
+
+
+    /* prob4:
+       Write a program to check whether a number is prime or not
+       Sample Output:
+       Input a number to check prime or not: 13
+       The entered number is a prime number.
+     */
+
+
+    /* prob5:
+       Write a program to find prime number within a range
+       Input number for starting range: 1
+       Input number for ending range: 100
+       The prime numbers between 1 and 100 are:
+       2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
+       The total number of prime numbers between 1 to 100 is: 25
+     */
+
+
+    /* prob6:
+       Write a program to find the factorial of a number
+       Sample output:
+       Input a number to find the factorial: 5
+       The factorial of the given number is: 120
+     */
+
+
+    /* prob7:
+       Write a program to find the last prime number occur before the entered number.
+       Sample output:
+       Input a number to find the last prime number occurs before the number: 50
+       47 is the last prime number before 50
+     */
+
+
+    /* prob8:
+       Write a program to find the Greatest Common Divisor (GCD) of two numbers.
+       Sample Output:
+       Input the first number: 25
+       Input the second number: 15
+       The Greatest Common Divisor is: 5
+     */
+
+
+    /* prob9:
+       Write a program to find the sum of digits of a given number.
+       Sample Output:
+       Input a number: 1234
+       The sum of digits of 1234 is: 10
+     */
+
+
+    /* prob10:
+       Write a program to find the sum of the series 1 + 1/2^2 + 1/3^3 + ..+ 1/n^n.
+       Sample Output:
+       Input the value for nth term: 5
+       1/1^1 = 1
+       1/2^2 = 0.25
+       1/3^3 = 0.037037
+       1/4^4 = 0.00390625
+       1/5^5 = 0.00032
+       The sum of the above series is: 1.29126
+     */
+
+
+
+
+
+
+
+}
